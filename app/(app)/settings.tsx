1316cc6597e4d848cadee1934a2f9da19478e5a0
Yury Odintsov <yury.odinson@gmail.com>
2026-02-27 13:16:41 +0300

    add: settings form

--- app/(app)/settings.tsx
+++ app/(app)/settings.tsx
@@ -1,9 +1,10 @@
 import AppButton from '@/components/AppButton';
 import { useThemeColor } from '@/hooks/use-theme-color';
 import useAuth from '@/store/auth';
+import useMe from '@/store/me';
 import useHandleTheme from '@/store/theme';
 import { Moon, Sun } from 'lucide-react-native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Switch, Text, TextInput, View } from 'react-native';
 import { authStyles } from '../(auth)/login';
 
@@ -13,12 +14,52 @@ export default function SettingsScreen() {
 	const theme = useHandleTheme((state) => state.theme);
 	const toggleTheme = useHandleTheme((state) => state.toggle);
 
-	const [name, setName] = useState<string>();
-	const [email, setEmail] = useState<string>();
-	const [password, setPassword] = useState<string>();
-	const [confirmPassword, setConfirmPassword] = useState<string>();
+	const me = useMe((state) => state.data);
+
+	const [name, setName] = useState<string>("");
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [confirmPassword, setConfirmPassword] = useState<string>("");
+	const [isFormDirty, setIsFormDirty] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (!me) {
+			setName("");
+			setEmail("");
+			setPassword("");
+			setConfirmPassword("");
+			setIsFormDirty(false);
+			return;
+		}
+
+		if (!isFormDirty) {
+			setName(me.name);
+			setEmail(me.email);
+		}
+	}, [isFormDirty, me]);
+
+	const handleNameChange = (value: string) => {
+		setIsFormDirty(true);
+		setName(value);
+	};
+
+	const handleEmailChange = (value: string) => {
+		setIsFormDirty(true);
+		setEmail(value);
+	};
+
+	const handlePasswordChange = (value: string) => {
+		setIsFormDirty(true);
+		setPassword(value);
+	};
+
+	const handleConfirmPasswordChange = (value: string) => {
+		setIsFormDirty(true);
+		setConfirmPassword(value);
+	};
 
 	const textColor = useThemeColor({}, "text");
+	const secondaryTextColor = useThemeColor({}, "secondaryText");
 	const borderColor = useThemeColor({}, "border");
 	const inputBgColor = useThemeColor({}, "inputBg");
 	const warningColor = useThemeColor({}, "warning");
@@ -41,42 +82,67 @@ export default function SettingsScreen() {
 
 			<View>
 
-				<Text style={[{ color: textColor }, styles.title]}>Аккаунт</Text>
+				<Text style={[{ color: textColor }, styles.subTitle]}>Аккаунт</Text>
+
 				<View style={[{ borderColor: borderColor, backgroundColor: inputBgColor }, styles.item]}>
-					<TextInput
-						style={[{ color: textColor, backgroundColor: inputBgColor, borderColor: borderColor }, authStyles.input]}
-						placeholder="Ваше имя"
-						value={name}
-						onChangeText={setName}
-						autoCapitalize="none"
-					/>
-					<TextInput
-						style={[{ color: textColor, backgroundColor: inputBgColor, borderColor: borderColor }, authStyles.input]}
-						placeholder="E-mail"
-						value={email}
-						onChangeText={setEmail}
-						keyboardType="email-address"
-						autoCapitalize="none"
-					/>
-					<TextInput
-						style={[{ color: textColor, backgroundColor: inputBgColor, borderColor: borderColor }, authStyles.input]}
-						placeholder="Пароль"
-						value={password}
-						onChangeText={setPassword}
-						secureTextEntry
-					/>
-					<TextInput
-						style={[{ color: textColor, backgroundColor: inputBgColor, borderColor: borderColor }, authStyles.input]}
-						placeholder="Повторите пароль"
-						value={confirmPassword}
-						onChangeText={setConfirmPassword}
-						secureTextEntry
-					/>
+
+					<View style={styles.inputLabel}>
+						{name !== "" &&
+							<Text style={[{ color: secondaryTextColor, backgroundColor: inputBgColor }, styles.inputPlaceholder]}>Имя</Text>
+						}
+						<TextInput
+							style={[{ color: textColor, backgroundColor: inputBgColor, borderColor: borderColor }, authStyles.input]}
+							placeholder="Ваше имя"
+							value={name}
+							onChangeText={handleNameChange}
+							autoCapitalize="none"
+						/>
+					</View>
+
+					<View style={styles.inputLabel}>
+						{email !== "" &&
+							<Text style={[{ color: secondaryTextColor, backgroundColor: inputBgColor }, styles.inputPlaceholder]}>Email</Text>
+						}
+						<TextInput
+							style={[{ color: textColor, backgroundColor: inputBgColor, borderColor: borderColor }, authStyles.input]}
+							placeholder="E-mail"
+							value={email}
+							onChangeText={handleEmailChange}
+							keyboardType="email-address"
+							autoCapitalize="none"
+						/>
+					</View>
+
+					<View style={styles.inputLabel}>
+						{password !== "" &&
+							<Text style={[{ color: secondaryTextColor, backgroundColor: inputBgColor }, styles.inputPlaceholder]}>Пароль</Text>
+						}
+						<TextInput
+							style={[{ color: textColor, backgroundColor: inputBgColor, borderColor: borderColor }, authStyles.input]}
+							placeholder="Пароль"
+							value={password}
+							onChangeText={handlePasswordChange}
+							secureTextEntry
+						/>
+					</View>
+
+					<View style={styles.inputLabel}>
+						{confirmPassword !== "" &&
+							<Text style={[{ color: secondaryTextColor, backgroundColor: inputBgColor }, styles.inputPlaceholder]}>Повторите пароль</Text>
+						}
+						<TextInput
+							style={[{ color: textColor, backgroundColor: inputBgColor, borderColor: borderColor }, authStyles.input]}
+							placeholder="Повторите пароль"
+							value={confirmPassword}
+							onChangeText={handleConfirmPasswordChange}
+							secureTextEntry
+						/>
+					</View>
 				</View>
 			</View>
 
 			<View>
-				<Text style={[{ color: textColor }, styles.title]}>Внешний вид</Text>
+				<Text style={[{ color: textColor }, styles.subTitle]}>Внешний вид</Text>
 				<View style={[{ borderColor: borderColor, backgroundColor: inputBgColor, flexDirection: "row" }, styles.item]}>
 					<Text style={[{ color: textColor }, { fontSize: 20 }]}>Тема оформления</Text>
 					<View style={{ marginLeft: "auto" }}>
@@ -106,16 +172,32 @@ const styles = StyleSheet.create({
 	},
 	title: {
 		marginBottom: 12,
-		fontSize: 24,
+		fontSize: 26,
+		fontWeight: 600
+	},
+	subTitle: {
+		marginBottom: 12,
+		fontSize: 22,
 		fontWeight: 600
 	},
 	item: {
 		padding: 8,
 		gap: 16,
-		// flexDirection: "row",
 		alignItems: "center",
 		justifyContent: "space-between",
 		borderWidth: 1,
 		borderRadius: 16,
+	},
+	inputLabel: {
+		position: "relative",
+		width: "100%",
+	},
+	inputPlaceholder: {
+		position: "absolute",
+		top: -8,
+		left: 8,
+		paddingHorizontal: 8,
+		fontSize: 12,
+		zIndex: 1
 	}
 });
